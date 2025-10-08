49bd89653d7e4f561daeba9e87371477c9b4818a
LindaleRock <lindalerock@gmail.com>
2025-10-08 08:32:22 +0600

    project setup completed

---
+++ src/Routes/Route.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Route = () => {
+  return (
+    <div>
+      <h2>this is for route</h2>
+    </div>
+  );
+};
+
+export default Rout2<h2>this is for route</h2>
---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div>
+      <h3>This is a footer</h3>
+    </div>
+  );
+};
+
+export default Footer;
+<h3>This is a footer</h3>;
